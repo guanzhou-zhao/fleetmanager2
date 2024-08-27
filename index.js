a021eb7914ffc7234257596d3e6f31d895c9a670
guanzhou zhao <benzhao2866@gmail.com>
2024-08-27 20:32:43 +1200

    init commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const helmet = require("helmet");
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
+const Firestore = require('@google-cloud/firestore');
+const { OAuth2Client } = require('google-auth-library');
+const path = require('path');
+
+
+var app = express();
+app.set('trust proxy', true);
+const helmetOpts = {
+  contentSecurityPolicy: {
+    directives: {
+      "connect-src": ["'self'", "https://accounts.google.com/gsi/"],
+      "script-src": ["'self'", "'unsafe-inline'", "https://accounts.google.com/gsi/client", "https://localhost:8443/script.js", "https://unpkg.com/axios@1.6.7/dist/axios.min.js", "https://unpkg.com/react@18/", "https://unpkg.com/react-dom@18/"],
+      "style-src": ["'self'", "'unsafe-inline'", "https://accounts.google.com/gsi/style", "https://use.fontawesome.com/releases/v5.8.2/css/all.css", "https://fonts.googleapis.com/css2"],
+      "img-src": ["'self'", "https://lh3.googleusercontent.com"],
+      "base-uri": ["https://accounts.google.com/gsi/", "https://localhost:8443/"],
+      "script-src-attr": ["'unsafe-inline'"],
+      "default-src": ["https://accounts.google.com/gsi/"]
+    },
+  },
+  crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" },
+}
+app.use(
+  helmet(helmetOpts),
+)
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser())
+
+
+app.use(express.static('public'))
+
+app.get('/', async (req, res) => {
+
+
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+})
+function validateTokenAndGetPayload(token, req) {
+
+}
+app.post('/user', async (req, res) => {
+  console.log('POST /user req.body', req.body)
+  let payload;
+
+  if (req.body && req.body.token) {
+
+    const client = new OAuth2Client();
+
+    let hasError = false, errorString, ticket
+    try {
+      ticket = await client.verifyIdToken({
+        idToken: req.body.token,
+        audience: '859003465245-0e19el21rsofb768u8icerklp5o8np6r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        maxExpiry: 3600 * 24 * 300,
+      })
+    } catch (error) {
+      console.log(typeof error, '--', error.toString())
+      hasError = true
+      errorString = error.toString()
+      if (errorString.indexOf('Token used too late') > -1) { // token used too late error
+        let matches = errorString.match(/\{.+\}/g)
+
+        if (matches && matches.length > 0) {
+          payload = JSON.parse(matches[0])
+          console.log('payload:', payload)
+        }
+      } else { // other error indicates invalid token
+        payload = { error: 'authenticate error' }
+      }
+    }
+    if (!hasError) {
+      payload = ticket.getPayload();
+      // If the request specified a Google Workspace domain:
+      // const domain = payload['hd'];        
+    }
+    console.log('token payload:', payload)
+  } else {
+    // no token in cookie
+    console.log('no token in req.body')
+  }
+
+
+
+  res.json({ ok: 1 })
+})
+
+const PORT = process.env.PORT || 8443
+if (process.env.PORT) {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+  });
+} else {
+  const fs = require('fs');
+  // const http = require('http');
+  const https = require('https');
+  const privateKey = fs.readFileSync('./sslcert/selfsigned.key', 'utf8');
+  const certificate = fs.readFileSync('./sslcert/selfsigned.crt', 'utf8');
+
+  const credentials = { key: privateKey, cert: certificate };
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(PORT, () => {
+    console.log(`https Server listening on port ${PORT}...`);
+  });
+}
---
+++ public/script.js
@@ -0,0 +1,30 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function decodeJwtResponse(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
